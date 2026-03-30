945cf7f1a396b20b93f81da341e7547e3e9b0187
Maurice <maurice.assoumanou507@gmail.com>
2026-03-30 12:08:01 +0200

    Add files via upload

--- log-analyzer1/log_analyzer.py
+++ log-analyzer1/log_analyzer.py
@@ -10,10 +10,15 @@ while True:
 				line_count += 1
 				if "error" in line.lower():
 					error_count += 1
+		print("\n--- Analyse abgeschlossen --")
 		print("Anzahl der Fehler: ", error_count)
 		print("Anzahl der untersuchten Zeilen: ", line_count)
+		if line_count > 0:
+			error_rate = (error_count / line_count) * 100
+			print("Fehlerrate: ", round(error_rate, 2), "%")
 		break
 	except FileNotFoundError:
 		print("Fehler: Datei nicht gefunden.")
 
 
+print("\nAnalyse erfolgreich abgeschlossen.")
